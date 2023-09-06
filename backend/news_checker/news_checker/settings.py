76a22f2f021dd1878ed32008acb95aaa3f1e4b9a
Xena <siapzena@gmail.com>
2023-09-06 23:41:49 +0300

    Working frontend

--- backend/news_checker/news_checker/settings.py
+++ backend/news_checker/news_checker/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'verifier',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'news_checker.urls'
@@ -122,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True  # For development purposes
--- frontend/news_checker_frontend/src/App.js
+++ frontend/news_checker_frontend/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [manualText, setManualText] = useState("");
+  const [result, setResult] = useState("");
+
+  const checkNews = async () => {
+    const response = await fetch('http://127.0.0.1:8000/api/check-news/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        url,
+        manual_text: manualText
+      }),
+    });
+
+    const data = await response.json();
+    setResult(data.result);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+        placeholder="Enter article URL"
+      />
+      <textarea
+        value={manualText}
+        onChange={(e) => setManualText(e.target.value)}
+        placeholder="Or manually paste the article here"
+      />
+      <button onClick={checkNews}>Check News</button>
+      <p>{result}</p>
     </div>
   );
 }
